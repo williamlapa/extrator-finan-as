ae9f710f20d53910b65070aedb35e2889a9934ef
williamlapa <williamlapa@hotmail.com>
2025-04-08 10:18:18 -0300

    tabela tesouro

--- app_prod.py
+++ app_prod.py
@@ -26,6 +26,7 @@ NETO_FILE_PATH = os.path.join(DOWNLOAD_FOLDER, NETO_FILE_NAME)
 
 # Configurações do Tesouro
 TESOURO_URL = "https://www.tesourotransparente.gov.br/ckan/dataset/df56aa42-484a-4a59-8184-7676580c81e3/resource/796d2059-14e9-44e3-80c9-2d9e30b405c1/download/PrecoTaxaTesouroDireto.csv"
+
 TESOURO_FILE_NAME = "PrecoTaxaTesouroDireto.csv"
 TESOURO_FILE_PATH = os.path.join(DOWNLOAD_FOLDER, TESOURO_FILE_NAME)
 
