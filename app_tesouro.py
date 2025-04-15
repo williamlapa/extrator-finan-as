62ccf0a8a873263b42679d0e663cbb0ec7f5e9d8
williamlapa <williamlapa@hotmail.com>
2025-04-15 19:06:44 -0300

    ajuste

--- app_tesouro.py
+++ app_tesouro.py
@@ -6,7 +6,7 @@ import numpy as np
 TESOURO_URL = "https://www.tesourotransparente.gov.br/ckan/dataset/df56aa42-484a-4a59-8184-7676580c81e3/resource/796d2059-14e9-44e3-80c9-2d9e30b405c1/download/PrecoTaxaTesouroDireto.csv"
 
 # Adicionar a imagem no topo
-st.image("tesouro_direto.jpeg", caption="Tesouro Direto", use_column_width=True)
+st.image("tesouro_direto.jpeg", caption="Tesouro Direto", use_container_width=True)
 
 # Título do app
 st.title("Análise de Variação do Tesouro Direto")
